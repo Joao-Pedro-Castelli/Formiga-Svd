06e98d4f8fd36326fc906dd0731ddcd828fdc041
Joao Pedro Castelli <jotapcastelli@gmail.com>
2026-07-06 17:58:21 -0300

    fix jacobi_decomposition; test working

--- src/svd/mod.rs
+++ src/svd/mod.rs
@@ -42,7 +42,7 @@ fn jacobi_decomposition<const R: usize>(
     let mut mat_v = SMatrix::<f64, R, R>::identity();
 
     for _ in 0..kmax {
-        let (max, index) = max_lower_element(a_mat);
+        let (max, index) = max_lower_element(&mat_ak);
         if max < tol {
             break;
         }
@@ -51,11 +51,12 @@ fn jacobi_decomposition<const R: usize>(
 
         let mut mat_u = SMatrix::identity();
         mat_u[(index.0, index.0)] = c;
-        mat_u[(index.1, index.1)] = c;
-        mat_u[index] = s;
+        mat_u[(index.0, index.1)] = s;
         mat_u[(index.1, index.0)] = -s;
+        mat_u[(index.1, index.1)] = c;
 
-        mat_v = mat_v * mat_u;
+        let tmp = mat_v * mat_u;
+        mat_v = tmp;
 
         mat_ak = mat_v.transpose() * a_mat * mat_v;
     }
@@ -87,12 +88,11 @@ mod tests {
 
         // vector needs to be sorted
         let eig_value_a = Vector3::new(15.833, 40.4584, 170.7086);
-        let (mut calc_eigv_a, _) = jacobi_decomposition::<3>(&mat_a, 0.00001, 1000);
+        let (mut calc_eigv_a, _) = jacobi_decomposition::<3>(&mat_a, 1e-12, 10000);
         calc_eigv_a
             .as_mut_slice()
-            .sort_by(|a, b| b.partial_cmp(a).unwrap());
-        println!("{}", calc_eigv_a);
-        assert!((eig_value_a - calc_eigv_a).apply_norm(&UniformNorm) < 0.001);
+            .sort_by(|a, b| a.partial_cmp(b).unwrap());
+        assert!((eig_value_a - calc_eigv_a).apply_norm(&UniformNorm) < 1e-3);
 
         // let eig_vec_b = Matrix3::new();
 
@@ -101,6 +101,6 @@ mod tests {
         calc_eigv_b
             .as_mut_slice()
             .sort_by(|a, b| a.partial_cmp(b).unwrap());
-        assert!((eig_value_b - calc_eigv_b).apply_norm(&UniformNorm) < 0.001);
+        assert!((eig_value_b - calc_eigv_b).apply_norm(&UniformNorm) < 1e-3);
     }
 }
